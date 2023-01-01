2992dfbd093405aabb5d1f905ad2ab8685277fe7
Chandra Deb <mail.chandradeb@gmail.com>
2023-01-01 18:19:48 +0600

    init

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const backgroundColor = Colors.black;
+final buttonColor = Colors.red.shade400;
+const borderColor = Colors.grey;
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+import 'constants.dart';
+import 'views/screens/auth/signup_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(GetMaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Flutter Demo',
+    theme: ThemeData.dark().copyWith(
+      scaffoldBackgroundColor: backgroundColor,
+    ),
+    home: const HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SignUpScreen();
+  }
+}
